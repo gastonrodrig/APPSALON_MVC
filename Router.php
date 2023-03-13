1e16ad07868ab03dca19d96babfec0dc6f0698c0
gastonrodrig04 <202111913@urp.edu.pe>
2023-03-13 03:41:10 -0500

    Cambio en el router 2

--- Router.php
+++ Router.php
@@ -28,20 +28,18 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        if ($_SERVER['PATH_INFO']) {
+        if (isset($_SERVER['PATH_INFO'])) {
             $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         } else {
             $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         }
-
+        
         $method = $_SERVER['REQUEST_METHOD'];
 
-        $splitURL = explode('?', $currentUrl);
-
         if ($method === 'GET') {
-            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables
+            $fn = $this->getRoutes[$currentUrl] ?? null;
         } else {
-            $fn = $this->postRoutes[$splitURL[0]] ?? null;
+            $fn = $this->postRoutes[$currentUrl] ?? null;
         }
 
         if ($fn) {
