20404c2705d56e45ccf9f994c3f8970651153b11
gastonrodrig04 <202111913@urp.edu.pe>
2023-03-18 16:51:30 -0500

    cambios 10000

--- Router.php
+++ Router.php
@@ -27,7 +27,7 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'];
+        $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
